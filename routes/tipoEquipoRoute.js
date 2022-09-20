1708490de116d23f87901349940cbecfc7c87d9c
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-09-20 15:01:27 -0500

    Creacion de las rutas de tipoEquipo

---
+++ routes/tipoEquipoRoute.js
@@ -0,0 +1,15 @@
+const {Router} = require('express');
+const {createTipoEquipo, 
+    getTipoEquipo, 
+    getTipoEquipoByID, 
+    updateTipoEquipoByID, 
+    deleteTipoEquipoByID } = require('../controllers/tipoEquipoController');
+
+    const router = Router();
+
+    router.post('/', createTipoEquipo);
+    router.get('/', getTipoEquipo);
+    router.get('/:id', getTipoEquipoByID);
+    router.put('/:id', updateTipoEquipoByID);
+    router.delete('/:id', deleteTipoEquipoByID);   
+    
