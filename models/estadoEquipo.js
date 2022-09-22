aba5693788f6046e69bc08d0a6cca0acc08e5ec1
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-09-22 15:21:59 -0500

    finalizacion de modelo estadoEquipo

---
+++ models/estadoEquipo.js
@@ -0,0 +1,23 @@
+const {Schema, model} = require('mongoose');
+
+const estadoEquipoSchema = Schema({
+    nombre:{ 
+        type: String, 
+        required: [true, 'El nombre es obligatorio'] 
+    },
+    estado:{ 
+        type: Boolean, 
+        default: true, 
+        required: true 
+    },
+    fechaCreacion:{ 
+        type: Date, 
+        default: new Date() 
+    },
+    fechaActualizacion:{ 
+        type: Date, 
+        default: new Date()
+ }
+});
+
+module.exports = model('EstadoEquipo', estadoEquipoSchema);
