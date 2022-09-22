6c077fb90af939cbac127bff1b737f159065e2ff
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-09-22 15:42:49 -0500

    Configuracion de rutas en app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 //To Do: Habilitar CORS
 
 const tipoEquipoRoute = require('./routes/tipoEquipoRoute'); //Importamos las rutas de tipoEquipo
-
+const usuarioRoute = require('./routes/usuarioRoute'); //Importamos las rutas de usuario
+const marcaRoute = require('./routes/marcaRoute');
+const estadoEquipoRoute = require('./routes/estadoEquipoRoute');
 /**
  * Middlewares: Es una función que se ejecuta antes de que llegue a la ruta
  */
@@ -14,6 +16,9 @@ app.use(express.json()); //Para poder leer los datos que vienen en formato JSON
 //To Do  middleware de subida de foto
 //To Do middleware de cors para que el servidor pueda ser consumido por cualquier cliente
 app.use('/api/tipoEquipo', tipoEquipoRoute); //Para que todas las rutas que empiecen con /api/tipoEquipo se dirijan a tipoEquipoRoute
+app.use('/api/usuario', usuarioRoute);
+app.use('/api/marca', marcaRoute);
+app.use('/api/estadoEquipo', estadoEquipoRoute);
 
 app.get('/',(req,res)=>{
     return res.json({})
