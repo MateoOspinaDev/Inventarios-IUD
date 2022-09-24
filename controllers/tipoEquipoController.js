25672e01f317e4727dc44eec744cd7908d6611ab
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-09-24 17:34:27 -0500

    Creacion de la logica de los controladores de tipomarca, tipoEquipo y estadoEquipo

--- controllers/tipoEquipoController.js
+++ controllers/tipoEquipoController.js
@@ -25,7 +25,8 @@ const getTipoEquipo = async (req=request, res=response) =>{
         console.log(req.query)
         const estado = req.query.estado//obtenemos el query de la peticion
         const query = {estado: estado} //Condicionamos la busqueda
-        const tipoequiposDB = await TipoEquipo.find()//En find podemos pasarle un objeto con las condiciones de busqueda
+        //Find tambien puede ir vacio, y traera todos los registros
+        const tipoequiposDB = await TipoEquipo.find(query)//En find podemos pasarle un objeto con las condiciones de busqueda
         return res.json(tipoequiposDB)
     }catch(e){
         console.log(e)
@@ -34,16 +35,45 @@ const getTipoEquipo = async (req=request, res=response) =>{
 
 }
 
-const getTipoEquipoByID = (req, res) =>{
-
+const getTipoEquipoByID = async (req, res) =>{
+    try{
+        const estado = req.query.estado
+        const id = req.params.id
+        const query = {estado: estado, _id: id}
+        const tipoequipoDB = await TipoEquipo.findOne(query)
+        return res.json(tipoequipoDB)
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({msg: e})  
+    }
 }
 
-const updateTipoEquipoByID = (req, res) =>{
-
+const updateTipoEquipoByID = async (req, res) =>{
+    try{
+        const id = req.params.id
+        const data = req.body;
+        data.FechaActualizacion = new Date();
+        const tipoEquipo = await TipoEquipo.findByIdAndUpdate(id, data, {new: true})
+        console.log(tipoEquipo)
+        return res.json(tipoEquipo)
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({msg: e})  
+    }
 }
 
-const deleteTipoEquipoByID = (req, res) =>{
-
+const deleteTipoEquipoByID = async (req, res) =>{
+    try{
+        const id = req.params.id
+        const tipoEquipo = await TipoEquipo.findByIdAndDelete(id)
+        if(!tipoEquipo){
+            return res.status(404).json({msg: 'No existe el tipo de equipo'})
+        }
+        return res.status(204).json({msg: 'Tipo de equipo eliminado'})
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({msg: e})  
+    }
 }
 
 module.exports = { 
---
+++ models/inventario.js
@@ -0,0 +1,54 @@
+const {Schema, model} = require('mongoose');
+
+//de esta forma creamos una coleccion (tabla en MySQL), ahí ingresamos los atributos.
+const InventarioSchema = Schema({
+        serial:{
+            type: String,
+            required: [true, 'El nombre es obligatorio'],
+            unique: true
+        },
+        modelo:{
+            type: Boolean,
+            required: [true, 'El nombre es obligatorio'],
+            unique: true
+        },
+        descripcion:{
+            type: string
+        },
+        foto:{
+            type: string
+        },
+        color:{
+            type: string
+        },
+        fechaCompra:{
+            type: Date,
+            default: new Date()
+        },
+        precio:{
+            type: Number
+        },
+        usuario:{
+            type: Schema.Types.ObjectId,
+            ref: 'Usuario',
+            required: true
+        },
+        marca:{ 
+            type: Schema.Types.ObjectId,
+            ref: 'Marca',
+            required: true
+        },
+        estado:{ 
+            type: Schema.Types.ObjectId,
+            ref: 'EstadoEquipo',
+            required: true
+        },
+        tipoEquipo:{ 
+            type: Schema.Types.ObjectId,
+            ref: 'TipoEquipo',
+            required: true
+        }
+});
+
+//Exportamos el modelo para poder usarlo en otros archivos
+module.exports = model('Inventario', InventarioSchema);
