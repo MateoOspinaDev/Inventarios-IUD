02bbb3d34036251292be2ff466789c6d47be4e5b
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-09-25 18:47:12 -0500

    Creacion endpoint Usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,22 +2,22 @@ const Usuario = require('../models/usuario');
 const {request, response} = require('express');//Para ser usado en los req y res de los metodos
 
 const createUsuario = async (req=request, res=response) =>{
-    const nombre = (req.body.nombre) 
-    ? req.body.nombre.toUpperCase()
-    : '';
-    const usuarioBD = await Usuario.findOne({nombre});
-
-    if (usuarioBD){
-        return res.status(400).json({
-            msg: `El usuario con ${nombre} ya existe`
-        });
-    }
-    const datos = {
-        nombre
-    }
-    const usuario = new Usuario(datos);
-    await usuario.save();
-    res.status(201).json(req.body);
+        try{
+            const data = req.body
+            const email = data.email
+            console.log(data)
+            const usuarioBD = await Usuario.findOne({ email })
+            if(usuarioBD){
+                return res.status(400).json({msg: 'Ya existe usuario'})
+            }
+            const usuario = new Usuario(data)
+            console.log(usuario)
+            await usuario.save()
+            return res.status(201).json(usuario)
+        }catch(e){
+            console.log(e)
+            return res.status(500).json({e})
+        }
 }
 
 const getUsuario = async (req, res) =>{
